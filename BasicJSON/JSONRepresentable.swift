8cb2735b0580488f0a294bca7ec52d1e80dea743
Theis Egeberg <theis.egebrerg@gmail.com>
2017-05-01 10:17:22 +0200

    Reduced complexity

--- BasicJSON/JSONRepresentable.swift
+++ BasicJSON/JSONRepresentable.swift
@@ -10,57 +10,94 @@ import Foundation
 
 public protocol JSONRepresentable {
     func jsonValue<T>() -> T
+    func asBool() -> Bool
+    func asString() -> String
+    func asDouble() -> Double
+    func asFloat() -> Float
+    func asInt() -> Int
+    func asPureJSON() -> PureJSON
+    func asPureJSONArray() -> [PureJSON]
 }
 
 extension JSONRepresentable {
+
+    public func asBool() -> Bool {
+        if let convertible = self as? Convertible {
+            return convertible.boolConverted
+        }
+        return false
+    }
+
+    public func asString() -> String {
+        if let convertible = self as? Convertible {
+            return convertible.stringConverted
+        }
+        return ""
+    }
+
+    public func asFloat() -> Float {
+        if let convertible = self as? Convertible {
+            return Float(convertible.doubleConverted)
+        }
+        return 0.0
+    }
+
+    public func asDouble() -> Double {
+        if let convertible = self as? Convertible {
+            return convertible.doubleConverted
+        }
+        return 0.0
+    }
+
+    public func asInt() -> Int {
+        if let convertible = self as? Convertible {
+            return convertible.intConverted
+        }
+        return 0
+    }
+
+    public func asPureJSON() -> PureJSON {
+        if let rawSelf = self as? RawJSON {
+            return PureJSON(raw:rawSelf)
+        }
+        return PureJSON()
+    }
+
+    public func asPureJSONArray() -> [PureJSON] {
+        if let rawList = self as? [RawJSON] {
+            return rawList.map({ (rawJSON) -> PureJSON in
+                return PureJSON(raw:rawJSON)
+            })
+        }
+        return [PureJSON]()
+    }
+
     public func jsonValue<T>() -> T {
         if let selfRef = self as? T {
             return selfRef
         }
-        if let boolValue = false as? T {
-            if let convertible = self as? Convertible, let converted = convertible.boolConverted as? T {
-                return converted
-            }
+        if let boolValue = self.asBool() as? T {
             return boolValue
         }
-        if let intValue = 0 as? T {
-            if let convertible = self as? Convertible, let converted = convertible.intConverted as? T {
-                return converted
-            }
+        if let intValue = self.asInt() as? T {
             return intValue
         }
-        if let floatValue = Float(0.0) as? T {
+        if let floatValue = self.asFloat() as? T {
             return floatValue
         }
-        if let doubleValue = Double(0.0) as? T {
-            if let convertible = self as? Convertible, let converted = convertible.doubleConverted as? T {
-                return converted
-            }
+        if let doubleValue = self.asDouble() as? T {
             return doubleValue
         }
 
-        if let jsonValue = PureJSON() as? T {
-            if let rawSelf = self as? RawJSON, let pureJSON = PureJSON(raw:rawSelf) as? T {
-                return pureJSON
-            }
+        if let jsonValue = self.asPureJSON() as? T {
             return jsonValue
         }
 
-        if let jsonValue = [PureJSON]() as? T {
-            if let rawList = self as? [RawJSON] {
-                if let pureList =  rawList.map({ (rawJSON) -> PureJSON in
-                    return PureJSON(raw:rawJSON)
-                }) as? T {
-                    return pureList
-                }
-            }
+        if let jsonValue = self.asPureJSONArray() as? T {
             return jsonValue
         }
 
-        if let stringValue = "" as? T {
-            if let convertible = self as? Convertible, let converted = convertible.stringConverted as? T {
-                return converted
-            }
+        if let stringValue = self.asString() as? T {
             return stringValue
         }
         fatalError("Something couldn't get converted")
