fae91fa08650a998f4733d16fe0eb4a2fdefe6a6
Theis Egeberg <theis.egebrerg@gmail.com>
2017-07-08 20:37:21 +0200

    MacOS targets added

---
+++ BasicJSONMacOS/BasicJSONMacOS.h
@@ -0,0 +1,19 @@
+//
+//  BasicJSONMacOS.h
+//  BasicJSONMacOS
+//
+//  Created by Theis Egeberg on 30/06/2017.
+//  Copyright © 2017 Theis Egeberg. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BasicJSONMacOS.
+FOUNDATION_EXPORT double BasicJSONMacOSVersionNumber;
+
+//! Project version string for BasicJSONMacOS.
+FOUNDATION_EXPORT const unsigned char BasicJSONMacOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BasicJSONMacOS/PublicHeader.h>
+
+
